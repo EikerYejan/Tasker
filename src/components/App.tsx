9ea6a71722af43f73304f24adeba70a2d291adb0
Eiker Yejan <eikeryejan@gmail.com>
2020-06-04 19:56:19 -0400

    Welcome component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 import Header from "./Header"
+import Welcome from "./Welcome"
 import "../assets/styles/App.scss"
 
 function App(): JSX.Element {
   return (
     <div id="app">
       <Header />
-      <main className="content">
-        <span className="app-loader loader" />
+      <main className="app__content">
+        <Welcome />
+        {/* <span className="app-loader loader" /> */}
       </main>
     </div>
   )
