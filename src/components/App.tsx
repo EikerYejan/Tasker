e23bf493f2523f78dce2cbce1505f8834bdf8b33
Eiker Yejan <eikeryejan@gmail.com>
2020-06-04 19:23:32 -0400

    Base layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,14 @@
 import React from "react"
+import Header from "./Header"
 import "../assets/styles/App.scss"
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <Header />
+      <main className="content">
+        <span className="app-loader loader" />
+      </main>
     </div>
   )
 }
