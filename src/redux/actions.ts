d95565e1be0e9c2b76a6d2ffd43434795e9c68e7
Eiker Yejan <eikeryejan@gmail.com>
2020-06-08 13:24:42 -0400

    Finish task feature

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,23 +1,30 @@
-import { Dispatch, Task } from "@types"
+import { Task, Dispatcher } from "@types"
 
 /**
  * Action types
  */
 const ADD_TASK = "ADD_TASK"
 const REMOVE_TASK = "REMOVE_TASK"
+const FINISH_TASK = "FINISH_TASK"
 
 /**
  * Add task
  * @param task - Task
  */
-const addTask = (task: Task) => (dispatch: Dispatch): void =>
+const addTask: Dispatcher<Task> = (task) => (dispatch): void =>
   dispatch({ type: ADD_TASK, payload: task })
 
 /**
  * Remove task
  * @param id
  */
-const deleteTask = (id: number) => (dispatch: Dispatch): void =>
+const deleteTask: Dispatcher<number> = (id) => (dispatch): void =>
   dispatch({ type: REMOVE_TASK, payload: id })
 
-export { addTask, deleteTask, ADD_TASK, REMOVE_TASK }
+/**
+ * Finish task
+ */
+const finishTask: Dispatcher<Task> = (task) => (dispatch): void =>
+  dispatch({ type: FINISH_TASK, payload: task })
+
+export { addTask, deleteTask, finishTask, ADD_TASK, REMOVE_TASK, FINISH_TASK }
