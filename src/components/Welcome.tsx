4e94ab9941f41cdcdac9c93498ef6ac9e3c448ca
Eiker Yejan <eikeryejan@gmail.com>
2020-06-09 19:08:58 -0400

    Copy text

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -11,7 +11,7 @@ const Welcome: React.FC<Props> = ({ name }) => (
   <div className="column is-12 welcome">
     <div className="welcome__content">
       <h1>{`Hello ${name}`}</h1>
-      <p>Welcome to Tasker, here you can manage your tasks and to-do items</p>
+      <p>Welcome to Tasker, here you can manage your tasks</p>
     </div>
   </div>
 )
