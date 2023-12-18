7e6f29e2f4683a2b3f0964ce810849c2b190822d
Eiker Yejan <eikeryejan@gmail.com>
2023-12-18 11:03:07 -0400

    temporarily remove lazy loading to fix web

--- src/MainNavigator.tsx
+++ src/MainNavigator.tsx
@@ -1,30 +1,13 @@
 import {createStackNavigator} from "@react-navigation/stack";
-import {Suspense, lazy} from "react";
-import {ActivityIndicator} from "react-native";
 
+import {HomeScreen} from "./screens/HomeScreen/HomeScreen";
+import {OnboardingScreen} from "./screens/OnboardingScreen";
+import {MenuScreen} from "./screens/MenuScreen";
 import {NavBar} from "./components/NavBar";
 
 import {useAppearance} from "./hooks/useAppearance";
 
 const Stack = createStackNavigator();
-
-const OnboardingScreen = lazy(() =>
-  import("./screens/OnboardingScreen").then(mod => ({
-    default: mod.OnboardingScreen,
-  })),
-);
-
-const HomeScreen = lazy(() =>
-  import("./screens/HomeScreen/HomeScreen").then(mod => ({
-    default: mod.HomeScreen,
-  })),
-);
-
-const MenuScreen = lazy(() =>
-  import("./screens/MenuScreen").then(mod => ({
-    default: mod.MenuScreen,
-  })),
-);
 
 export const MainNavigator = () => {
   useAppearance();
@@ -35,32 +18,16 @@ export const MainNavigator = () => {
       screenOptions={{
         header: () => <NavBar />,
       }}>
-      <Stack.Screen name="OnBoarding">
-        {props => (
-          <Suspense fallback={<ActivityIndicator />}>
-            <OnboardingScreen {...props} />
-          </Suspense>
-        )}
-      </Stack.Screen>
-      <Stack.Screen name="Home">
-        {() => (
-          <Suspense fallback={<ActivityIndicator />}>
-            <HomeScreen />
-          </Suspense>
-        )}
-      </Stack.Screen>
+      <Stack.Screen component={OnboardingScreen} name="OnBoarding" />
+      <Stack.Screen component={HomeScreen} name="Home" />
       <Stack.Screen
+        component={MenuScreen}
         name="Menu"
         options={{
           headerShown: false,
           presentation: "modal",
-        }}>
-        {() => (
-          <Suspense fallback={<ActivityIndicator />}>
-            <MenuScreen />
-          </Suspense>
-        )}
-      </Stack.Screen>
+        }}
+      />
     </Stack.Navigator>
   );
 };
