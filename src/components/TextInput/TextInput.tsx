37084f3264e60ecb70c9b5374289294bcf754cf0
Eiker Yejan <eikeryejan@gmail.com>
2023-12-22 19:00:33 -0400

    Refactor TextInput component to support secureTextEntry and password visibility toggle

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -2,34 +2,69 @@ import {
   StyleSheet,
   TextInput as RNTextInput,
   type TextInputProps,
+  View,
+  TouchableOpacity,
 } from "react-native";
+import {forwardRef, useState} from "react";
+import Icon from "react-native-vector-icons/Ionicons";
 
 import {FONTS} from "../../constants/fonts";
 
 import {useAppearance} from "../../hooks/useAppearance";
 
-interface Props extends TextInputProps {}
+export const TextInput = forwardRef<RNTextInput, TextInputProps>(
+  function TextInputWithRef({style, secureTextEntry, ...props}, ref) {
+    const [passwordVisible, setPasswordVisible] = useState(false);
 
-export const TextInput = ({style, ...props}: Props) => {
-  const {colors} = useAppearance();
+    const onPasswordIconPress = () => {
+      setPasswordVisible(!passwordVisible);
+    };
 
-  const styles = StyleSheet.create({
-    input: {
-      borderColor: colors.primaryInverse,
-      borderWidth: 1,
-      color: colors.text,
-      fontFamily: FONTS.POPPINS_REGULAR,
-      minHeight: 40,
-      padding: 10,
-      width: "100%",
-    },
-  });
+    const {colors} = useAppearance();
 
-  return (
-    <RNTextInput
-      {...props}
-      style={[styles.input, style]}
-      placeholderTextColor={colors.text}
-    />
-  );
-};
+    const styles = StyleSheet.create({
+      container: {
+        width: "100%",
+      },
+      passwordIcon: {
+        position: "absolute",
+        right: 10,
+        top: 6,
+      },
+      input: {
+        borderColor: colors.primaryInverse,
+        borderWidth: 1,
+        color: colors.text,
+        fontFamily: FONTS.POPPINS_REGULAR,
+        minHeight: 40,
+        padding: 10,
+        width: "100%",
+      },
+    });
+
+    console.log();
+
+    return (
+      <View style={[styles.container, style]}>
+        <RNTextInput
+          {...props}
+          ref={ref}
+          secureTextEntry={!!(secureTextEntry && !passwordVisible)}
+          style={styles.input}
+          placeholderTextColor={colors.text}
+        />
+        {secureTextEntry ? (
+          <TouchableOpacity
+            style={styles.passwordIcon}
+            onPress={onPasswordIconPress}>
+            <Icon
+              name={!passwordVisible ? "eye-outline" : "eye-off-outline"}
+              size={25}
+              color={colors.text}
+            />
+          </TouchableOpacity>
+        ) : null}
+      </View>
+    );
+  },
+);
