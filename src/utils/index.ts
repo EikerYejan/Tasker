00a91b618be1e960e3cb01b7cf639bb95aa5e4a8
Eiker Yejan <eikeryejan@gmail.com>
2023-12-22 19:12:46 -0400

    Add isEmailValid function to validate email addresses

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,11 @@
 export const generateId = () => {
   return Date.now(); //! unique-ish for now
 };
+
+export const isEmailValid = (email: string) => {
+  return !!String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+};
