b261be95ef92dbc55a0fb45f477f3d73635612f2
Eiker Yejan <eikeryejan@gmail.com>
2023-12-28 14:26:38 -0400

    Refactor AppleAuthButton component

--- src/components/AppleAuthButton/AppleAuthButton.tsx
+++ src/components/AppleAuthButton/AppleAuthButton.tsx
@@ -1,1 +1,31 @@
-export * from "./AppleAuthButton.native";
+import {StyleSheet, TouchableOpacity} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+
+import {useAppearance} from "../../hooks/useAppearance";
+
+interface Props {
+  onPress: () => void;
+}
+
+export const AppleAuthButton = ({onPress}: Props) => {
+  const {colors} = useAppearance();
+
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: "center",
+      backgroundColor: "transparent",
+      borderColor: colors.text,
+      borderWidth: 1,
+      flexDirection: "row",
+      height: 50,
+      justifyContent: "center",
+      width: 50,
+    },
+  });
+
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <Icon name="logo-apple" size={25} color={colors.text} />
+    </TouchableOpacity>
+  );
+};
